536493d1670bf9e2cd3531fd761dc2f1e42e69a2
Massimiliano Ziccardi <mziccard@redhat.com>
2020-10-02 13:22:40 +0200

    feat: 🎸 changed to functional and added depend on feature
    Now the component is implemented as functional component and has a new
    ability that allow to set dependencies on other fields. This way
    validation can be performed depending on other field values.

--- src/application/ApplicationDetail/panels/UPSForm/UPSFormField.tsx
+++ src/application/ApplicationDetail/panels/UPSForm/UPSFormField.tsx
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FormGroup, TextArea, TextInput } from '@patternfly/react-core';
-import { Validator } from 'json-data-validator';
+import { Data, Validator } from 'json-data-validator';
 import { MultiEvaluationResult } from 'json-data-validator/build/src/Rule';
 import { validatorToPF4Status } from '../../../../utils/ValidatorUtils';
 
@@ -14,78 +14,95 @@ interface Props {
   defaultValue?: string;
   component?: 'textarea' | 'textinput';
   validator?: Validator;
+  formData?: Data;
+  dependsOn?: string;
 }
 
-interface State {
-  formValidation?: MultiEvaluationResult | null;
-}
+export function UPSFormField({
+  fieldId,
+  label,
+  helperText,
+  helperTextInvalid,
+  validated,
+  onChange,
+  defaultValue,
+  component: componentType,
+  validator,
+  formData = {},
+  dependsOn,
+}: Props) {
+  const [
+    validationState,
+    setValidationState,
+  ] = useState<MultiEvaluationResult | null>(null);
 
-export class UPSFormField extends Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = {};
-  }
+  const [value, setValue] = useState<string | undefined>(undefined);
+  const field = formData[dependsOn || '__NO_DEPS'];
 
-  readonly render = () => {
-    const onChange = (newVal: string) => {
-      if (this.props.validator) {
-        const validation = this.props.validator.validate(
-          { [this.props.fieldId]: newVal },
-          true
-        ) as MultiEvaluationResult;
-        this.setState({ formValidation: validation });
-        if (this.props.onChange) {
-          this.props.onChange(newVal);
-        }
+  useEffect(() => {
+    if (validator && value !== undefined) {
+      const validation = validator.validate(
+        { ...formData, [fieldId]: value || '' },
+        true
+      ) as MultiEvaluationResult;
+      setValidationState(validation);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [value]);
+
+  useEffect(() => {
+    if (validator && (field || !!validationState)) {
+      const validation = validator.validate(
+        { ...formData, [fieldId]: value || '' },
+        true
+      ) as MultiEvaluationResult;
+      setValidationState(validation);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [field]);
+
+  const onInputChange = (newVal: string) => {
+    if (validator) {
+      setValue(newVal);
+      if (onChange) {
+        onChange(newVal);
       }
-    };
-
-    const component = () => {
-      if (this.props.component === 'textarea') {
-        return (
-          <TextArea
-            type="text"
-            id={`ta-${this.props.fieldId}`}
-            defaultValue={this.props.defaultValue}
-            onChange={onChange}
-            validated={validatorToPF4Status(
-              this.state.formValidation,
-              this.props.fieldId
-            )}
-          />
-        );
-      } else {
-        return (
-          <TextInput
-            type="text"
-            id={`ti-${this.props.fieldId}`}
-            defaultValue={this.props.defaultValue}
-            onChange={onChange}
-            validated={validatorToPF4Status(
-              this.state.formValidation,
-              this.props.fieldId
-            )}
-          />
-        );
-      }
-    };
-
-    return (
-      <FormGroup
-        fieldId={this.props.fieldId}
-        label={this.props.label}
-        helperText={this.props.helperText}
-        helperTextInvalid={
-          this.state.formValidation?.getEvaluationResult(this.props.fieldId)
-            ?.message
-        }
-        validated={validatorToPF4Status(
-          this.state.formValidation,
-          this.props.fieldId
-        )}
-      >
-        {component()}
-      </FormGroup>
-    );
+    }
   };
+
+  const component = () => {
+    if (componentType === 'textarea') {
+      return (
+        <TextArea
+          type="text"
+          id={`ta-${fieldId}`}
+          defaultValue={defaultValue}
+          onChange={onInputChange}
+          validated={validatorToPF4Status(validationState, fieldId)}
+        />
+      );
+    } else {
+      return (
+        <TextInput
+          type="text"
+          id={`ti-${fieldId}`}
+          defaultValue={defaultValue}
+          onChange={onInputChange}
+          validated={validatorToPF4Status(validationState, fieldId)}
+        />
+      );
+    }
+  };
+
+  return (
+    <FormGroup
+      fieldId={fieldId}
+      label={label}
+      helperText={helperText}
+      helperTextInvalid={validationState?.getEvaluationResult(fieldId)?.message}
+      validated={validatorToPF4Status(validationState, fieldId)}
+    >
+      {component()}
+    </FormGroup>
+  );
 }
